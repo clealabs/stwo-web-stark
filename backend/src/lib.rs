64dbcdf29e9e0b0cee38cfb85430bb7a6364a777
Vincent Palma <vincent@palma.dev>
2025-08-09 23:37:53 +0200

    successful compilation with verifier

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,174 +1,174 @@
 mod utils;
 
 use cairo_air::{air::CairoProof, verifier::verify_cairo, PreProcessedTraceVariant};
-use cairo_vm::{
-    cairo_run::{self},
-    hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor,
-    types::layout_name::LayoutName,
-    vm::{
-        errors::cairo_run_errors::CairoRunError,
-        runners::{
-            cairo_pie::{
-                CairoPie, CairoPieAdditionalData, CairoPieMemory, CairoPieMetadata, CairoPieVersion,
-            },
-            cairo_runner::{ExecutionResources, RunResources},
-        },
-    },
-};
-use serde::{Deserialize, Serialize};
-use stwo_cairo_adapter::{vm_import::VmImportError, ProverInput};
+// use cairo_vm::{
+//     cairo_run::{self},
+//     hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor,
+//     types::layout_name::LayoutName,
+//     vm::{
+//         errors::cairo_run_errors::CairoRunError,
+//         // runners::{
+//         //     cairo_pie::{
+//         //         CairoPie, CairoPieAdditionalData, CairoPieMemory, CairoPieMetadata,
+// CairoPieVersion,         //     },
+//         //     cairo_runner::{ExecutionResources, RunResources},
+//         // },
+//     },
+// };
+// use serde::{Deserialize, Serialize};
+// use stwo_cairo_adapter::{vm_import::VmImportError, ProverInput};
 use stwo_cairo_prover::{
-    prover::prove_cairo,
+    // prover::prove_cairo,
     stwo_prover::core::{
         pcs::PcsConfig,
-        prover::ProvingError,
+        // prover::ProvingError,
         vcs::blake2_merkle::{Blake2sMerkleChannel, Blake2sMerkleHasher},
     },
 };
 use thiserror_no_std::Error;
-use utils::{adapt_finished_runner, set_panic_hook};
-use wasm_bindgen::prelude::*;
+// use utils::{adapt_finished_runner, set_panic_hook};
+// use wasm_bindgen::prelude::*;
 
-extern crate alloc;
+// extern crate alloc;
 
-#[cfg(target_arch = "wasm32")]
-use lol_alloc::{FreeListAllocator, LockedAllocator};
+// #[cfg(target_arch = "wasm32")]
+// use lol_alloc::{FreeListAllocator, LockedAllocator};
 
-#[cfg(target_arch = "wasm32")]
-#[global_allocator]
-static ALLOCATOR: LockedAllocator<FreeListAllocator> =
-    LockedAllocator::new(FreeListAllocator::new());
+// #[cfg(target_arch = "wasm32")]
+// #[global_allocator]
+// static ALLOCATOR: LockedAllocator<FreeListAllocator> =
+//     LockedAllocator::new(FreeListAllocator::new());
 
-pub struct TraceGenOutput {
-    pub execution_resources: ExecutionResources,
-    pub prover_input: ProverInput,
-}
+// pub struct TraceGenOutput {
+//     pub execution_resources: ExecutionResources,
+//     pub prover_input: ProverInput,
+// }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct TraceGenOutputJS {
-    execution_resources: String,
-    prover_input: String,
-}
+// #[derive(Debug, Serialize, Deserialize)]
+// pub struct TraceGenOutputJS {
+//     execution_resources: String,
+//     prover_input: String,
+// }
 
-pub fn from_zip_archive<R: std::io::Read + std::io::Seek>(
-    mut zip_reader: zip::ZipArchive<R>,
-) -> Result<CairoPie, std::io::Error> {
-    use std::io::Read;
+// pub fn from_zip_archive<R: std::io::Read + std::io::Seek>(
+//     mut zip_reader: zip::ZipArchive<R>,
+// ) -> Result<CairoPie, std::io::Error> {
+//     use std::io::Read;
 
-    let version = match zip_reader.by_name("version.json") {
-        Ok(version_buffer) => {
-            let reader = std::io::BufReader::new(version_buffer);
-            serde_json::from_reader(reader)?
-        }
-        Err(_) => CairoPieVersion { cairo_pie: () },
-    };
+//     let version = match zip_reader.by_name("version.json") {
+//         Ok(version_buffer) => {
+//             let reader = std::io::BufReader::new(version_buffer);
+//             serde_json::from_reader(reader)?
+//         }
+//         Err(_) => CairoPieVersion { cairo_pie: () },
+//     };
 
-    let reader = std::io::BufReader::new(zip_reader.by_name("metadata.json")?);
-    let metadata: CairoPieMetadata = serde_json::from_reader(reader)?;
+//     let reader = std::io::BufReader::new(zip_reader.by_name("metadata.json")?);
+//     let metadata: CairoPieMetadata = serde_json::from_reader(reader)?;
 
-    let mut memory = vec![];
-    zip_reader.by_name("memory.bin")?.read_to_end(&mut memory)?;
-    let memory = CairoPieMemory::from_bytes(&memory)
-        .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::InvalidData))?;
+//     let mut memory = vec![];
+//     zip_reader.by_name("memory.bin")?.read_to_end(&mut memory)?;
+//     let memory = CairoPieMemory::from_bytes(&memory)
+//         .ok_or_else(|| std::io::Error::from(std::io::ErrorKind::InvalidData))?;
 
-    let reader = std::io::BufReader::new(zip_reader.by_name("execution_resources.json")?);
-    let execution_resources: ExecutionResources = serde_json::from_reader(reader)?;
+//     let reader = std::io::BufReader::new(zip_reader.by_name("execution_resources.json")?);
+//     let execution_resources: ExecutionResources = serde_json::from_reader(reader)?;
 
-    let reader = std::io::BufReader::new(zip_reader.by_name("additional_data.json")?);
-    let additional_data: CairoPieAdditionalData = serde_json::from_reader(reader)?;
+//     let reader = std::io::BufReader::new(zip_reader.by_name("additional_data.json")?);
+//     let additional_data: CairoPieAdditionalData = serde_json::from_reader(reader)?;
 
-    Ok(CairoPie {
-        metadata,
-        memory,
-        execution_resources,
-        additional_data,
-        version,
-    })
-}
+//     Ok(CairoPie {
+//         metadata,
+//         memory,
+//         execution_resources,
+//         additional_data,
+//         version,
+//     })
+// }
 
-#[wasm_bindgen]
-pub fn run_trace_gen(pie_zip_js: JsValue) -> Result<JsValue, JsValue> {
-    set_panic_hook();
+// #[wasm_bindgen]
+// pub fn run_trace_gen(pie_zip_js: JsValue) -> Result<JsValue, JsValue> {
+//     set_panic_hook();
 
-    let input: Vec<u8> = serde_wasm_bindgen::from_value(pie_zip_js)?;
-    let reader = std::io::Cursor::new(input);
-    let zip_archive = zip::ZipArchive::new(reader).unwrap();
+//     let input: Vec<u8> = serde_wasm_bindgen::from_value(pie_zip_js)?;
+//     let reader = std::io::Cursor::new(input);
+//     let zip_archive = zip::ZipArchive::new(reader).unwrap();
 
-    let pie = from_zip_archive(zip_archive)
-        .map_err(|e| JsValue::from(format!("Failed to deserialize pie: {e}")))?;
-    let trace_gen_output =
-        trace_gen(pie).map_err(|e| JsValue::from(format!("Failed to generate trace: {e}")))?;
-    Ok(serde_wasm_bindgen::to_value(&TraceGenOutputJS {
-        prover_input: serde_json::to_string(&trace_gen_output.prover_input)
-            .map_err(|e| JsValue::from(format!("Failed to serialize prover input: {e}")))?,
-        execution_resources: serde_json::to_string(&trace_gen_output.execution_resources)
-            .map_err(|e| JsValue::from(format!("Failed to serialize execution resources: {e}")))?,
-    })?)
-}
+//     let pie = from_zip_archive(zip_archive)
+//         .map_err(|e| JsValue::from(format!("Failed to deserialize pie: {e}")))?;
+//     let trace_gen_output =
+//         trace_gen(pie).map_err(|e| JsValue::from(format!("Failed to generate trace: {e}")))?;
+//     Ok(serde_wasm_bindgen::to_value(&TraceGenOutputJS {
+//         prover_input: serde_json::to_string(&trace_gen_output.prover_input)
+//             .map_err(|e| JsValue::from(format!("Failed to serialize prover input: {e}")))?,
+//         execution_resources: serde_json::to_string(&trace_gen_output.execution_resources)
+//             .map_err(|e| JsValue::from(format!("Failed to serialize execution resources:
+// {e}")))?,     })?)
+// }
 
-#[wasm_bindgen]
-pub fn run_prove(prover_input_js: JsValue) -> Result<JsValue, JsValue> {
-    set_panic_hook();
+// #[wasm_bindgen]
+// pub fn run_prove(prover_input_js: JsValue) -> Result<JsValue, JsValue> {
+//     set_panic_hook();
 
-    let prover_input: ProverInput =
-        serde_json::from_str(&serde_wasm_bindgen::from_value::<String>(prover_input_js)?)
-            .map_err(|e| JsValue::from(format!("Failed to deserialize prover input: {e}")))?;
-    let proof =
-        prove(prover_input).map_err(|e| JsValue::from(format!("Failed to generate proof: {e}")))?;
-    Ok(serde_wasm_bindgen::to_value(
-        &serde_json::to_string(&proof)
-            .map_err(|e| JsValue::from(format!("Failed to serialize proof: {e}")))?,
-    )?)
-}
+//     let prover_input: ProverInput =
+//         serde_json::from_str(&serde_wasm_bindgen::from_value::<String>(prover_input_js)?)
+//             .map_err(|e| JsValue::from(format!("Failed to deserialize prover input: {e}")))?;
+//     let proof =
+//         prove(prover_input).map_err(|e| JsValue::from(format!("Failed to generate proof:
+// {e}")))?;     Ok(serde_wasm_bindgen::to_value(
+//         &serde_json::to_string(&proof)
+//             .map_err(|e| JsValue::from(format!("Failed to serialize proof: {e}")))?,
+//     )?)
+// }
 
-#[wasm_bindgen]
-pub fn run_verify(proof_js: JsValue) -> Result<JsValue, JsValue> {
-    set_panic_hook();
+// #[wasm_bindgen]
+// pub fn run_verify(proof_js: JsValue) -> Result<JsValue, JsValue> {
+//     set_panic_hook();
 
-    let proof: CairoProof<Blake2sMerkleHasher> =
-        serde_json::from_str(&serde_wasm_bindgen::from_value::<String>(proof_js)?)
-            .map_err(|e| JsValue::from(format!("Failed to deserialize proof: {e}")))?;
-    let verdict = verify(proof);
-    Ok(serde_wasm_bindgen::to_value(&verdict)?)
-}
+//     let proof: CairoProof<Blake2sMerkleHasher> =
+//         serde_json::from_str(&serde_wasm_bindgen::from_value::<String>(proof_js)?)
+//             .map_err(|e| JsValue::from(format!("Failed to deserialize proof: {e}")))?;
+//     let verdict = verify(proof);
+//     Ok(serde_wasm_bindgen::to_value(&verdict)?)
+// }
 
-pub fn trace_gen(pie: CairoPie) -> Result<TraceGenOutput, VmError> {
-    let cairo_run_config = cairo_run::CairoRunConfig {
-        trace_enabled: true,
-        relocate_mem: true,
-        layout: LayoutName::all_cairo_stwo,
-        ..Default::default()
-    };
+// pub fn trace_gen(pie: CairoPie) -> Result<TraceGenOutput, VmError> {
+//     let cairo_run_config = cairo_run::CairoRunConfig {
+//         trace_enabled: true,
+//         relocate_mem: true,
+//         layout: LayoutName::all_cairo_stwo,
+//         ..Default::default()
+//     };
 
-    let mut hint_processor = BuiltinHintProcessor::new(
-        Default::default(),
-        RunResources::new(pie.execution_resources.n_steps),
-    );
-    let cairo_runner_result =
-        cairo_run::cairo_run_pie(&pie, &cairo_run_config, &mut hint_processor);
+//     let mut hint_processor = BuiltinHintProcessor::new(
+//         Default::default(),
+//         RunResources::new(pie.execution_resources.n_steps),
+//     );
+//     let cairo_runner_result =
+//         cairo_run::cairo_run_pie(&pie, &cairo_run_config, &mut hint_processor);
 
-    let cairo_runner = match cairo_runner_result {
-        Ok(runner) => runner,
-        Err(error) => {
-            return Err(VmError::Runner(error));
-        }
-    };
+//     let cairo_runner = match cairo_runner_result {
+//         Ok(runner) => runner,
+//         Err(error) => {
+//             return Err(VmError::Runner(error));
+//         }
+//     };
 
-    Ok(TraceGenOutput {
-        execution_resources: cairo_runner
-            .get_execution_resources()
-            .map_err(|e| VmError::Runner(CairoRunError::Runner(e)))?,
-        prover_input: adapt_finished_runner(cairo_runner)?,
-    })
-}
+//     Ok(TraceGenOutput {
+//         execution_resources: cairo_runner
+//             .get_execution_resources()
+//             .map_err(|e| VmError::Runner(CairoRunError::Runner(e)))?,
+//         prover_input: adapt_finished_runner(cairo_runner)?,
+//     })
+// }
 
-pub fn prove(prover_input: ProverInput) -> Result<CairoProof<Blake2sMerkleHasher>, ProvingError> {
-    prove_cairo::<Blake2sMerkleChannel>(
-        prover_input,
-        PcsConfig::default(),
-        PreProcessedTraceVariant::CanonicalWithoutPedersen,
-    )
-}
+// pub fn prove(prover_input: ProverInput) -> Result<CairoProof<Blake2sMerkleHasher>, ProvingError>
+// {     prove_cairo::<Blake2sMerkleChannel>(
+//         prover_input,
+//         PcsConfig::default(),
+//         PreProcessedTraceVariant::CanonicalWithoutPedersen,
+//     )
+// }
 
 pub fn verify(cairo_proof: CairoProof<Blake2sMerkleHasher>) -> bool {
     verify_cairo::<Blake2sMerkleChannel>(
@@ -183,8 +183,8 @@ pub fn verify(cairo_proof: CairoProof<Blake2sMerkleHasher>) -> bool {
 pub enum VmError {
     #[error("Failed to interact with the file system")]
     IO(#[from] std::io::Error),
-    #[error("The cairo program execution failed")]
-    Runner(#[from] CairoRunError),
-    #[error("The adapter execution failed")]
-    Adapter(#[from] VmImportError),
+    // #[error("The cairo program execution failed")]
+    // Runner(#[from] CairoRunError),
+    // #[error("The adapter execution failed")]
+    // Adapter(#[from] VmImportError),
 }
--- backend/src/utils.rs
+++ backend/src/utils.rs
@@ -1,72 +1,72 @@
-use cairo_vm::vm::runners::cairo_runner::CairoRunner;
-use itertools::Itertools;
-use stwo_cairo_adapter::{
-    builtins::MemorySegmentAddresses,
-    memory::{MemoryBuilder, MemoryConfig, MemoryEntry},
-    vm_import::{adapt_to_stwo_input, RelocatedTraceEntry, VmImportError},
-    HashMap, ProverInput, PublicSegmentContext,
-};
+// use cairo_vm::vm::runners::cairo_runner::CairoRunner;
+// use itertools::Itertools;
+// use stwo_cairo_adapter::{
+//     builtins::MemorySegmentAddresses,
+//     memory::{MemoryBuilder, MemoryConfig, MemoryEntry},
+//     vm_import::{adapt_to_stwo_input, RelocatedTraceEntry, VmImportError},
+//     HashMap, ProverInput, PublicSegmentContext,
+// };
 
-pub fn set_panic_hook() {
-    // When the `console_error_panic_hook` feature is enabled, we can call the
-    // `set_panic_hook` function at least once during initialization, and then
-    // we will get better error messages if our code ever panics.
-    //
-    // For more details see
-    // https://github.com/rustwasm/console_error_panic_hook#readme
-    #[cfg(feature = "console_error_panic_hook")]
-    console_error_panic_hook::set_once();
-}
+// pub fn set_panic_hook() {
+//     // When the `console_error_panic_hook` feature is enabled, we can call the
+//     // `set_panic_hook` function at least once during initialization, and then
+//     // we will get better error messages if our code ever panics.
+//     //
+//     // For more details see
+//     // https://github.com/rustwasm/console_error_panic_hook#readme
+//     #[cfg(feature = "console_error_panic_hook")]
+//     console_error_panic_hook::set_once();
+// }
 
-pub fn adapt_finished_runner(runner: CairoRunner) -> Result<ProverInput, VmImportError> {
-    let memory_iter = runner
-        .relocated_memory
-        .iter()
-        .enumerate()
-        .filter_map(|(i, v)| {
-            v.map(|v| MemoryEntry {
-                address: i as u64,
-                value: bytemuck::cast(v.to_bytes_le()),
-            })
-        });
+// pub fn adapt_finished_runner(runner: CairoRunner) -> Result<ProverInput, VmImportError> {
+//     let memory_iter = runner
+//         .relocated_memory
+//         .iter()
+//         .enumerate()
+//         .filter_map(|(i, v)| {
+//             v.map(|v| MemoryEntry {
+//                 address: i as u64,
+//                 value: bytemuck::cast(v.to_bytes_le()),
+//             })
+//         });
 
-    let public_input = runner.get_air_public_input()?;
+//     let public_input = runner.get_air_public_input()?;
 
-    let trace_iter = match runner.relocated_trace {
-        Some(ref trace) => trace.iter().map(|t| RelocatedTraceEntry {
-            ap: t.ap,
-            pc: t.pc,
-            fp: t.fp,
-        }),
-        None => return Err(VmImportError::TraceNotRelocated),
-    };
+//     let trace_iter = match runner.relocated_trace {
+//         Some(ref trace) => trace.iter().map(|t| RelocatedTraceEntry {
+//             ap: t.ap,
+//             pc: t.pc,
+//             fp: t.fp,
+//         }),
+//         None => return Err(VmImportError::TraceNotRelocated),
+//     };
 
-    let memory_segments: &HashMap<&str, MemorySegmentAddresses> = &public_input
-        .memory_segments
-        .into_iter()
-        .map(|(k, v)| {
-            (
-                k,
-                MemorySegmentAddresses {
-                    begin_addr: v.begin_addr,
-                    stop_ptr: v.stop_ptr,
-                },
-            )
-        })
-        .collect();
+//     let memory_segments: &HashMap<&str, MemorySegmentAddresses> = &public_input
+//         .memory_segments
+//         .into_iter()
+//         .map(|(k, v)| {
+//             (
+//                 k,
+//                 MemorySegmentAddresses {
+//                     begin_addr: v.begin_addr,
+//                     stop_ptr: v.stop_ptr,
+//                 },
+//             )
+//         })
+//         .collect();
 
-    let public_memory_addresses = public_input
-        .public_memory
-        .iter()
-        .map(|s| s.address as u32)
-        .collect_vec();
+//     let public_memory_addresses = public_input
+//         .public_memory
+//         .iter()
+//         .map(|s| s.address as u32)
+//         .collect_vec();
 
-    // TODO(spapini): Add output builtin to public memory.
-    adapt_to_stwo_input(
-        &trace_iter.collect_vec(),
-        MemoryBuilder::from_iter(MemoryConfig::default(), memory_iter),
-        public_memory_addresses,
-        memory_segments,
-        PublicSegmentContext::bootloader_context(),
-    )
-}
+//     // TODO(spapini): Add output builtin to public memory.
+//     adapt_to_stwo_input(
+//         &trace_iter.collect_vec(),
+//         MemoryBuilder::from_iter(MemoryConfig::default(), memory_iter),
+//         public_memory_addresses,
+//         memory_segments,
+//         PublicSegmentContext::bootloader_context(),
+//     )
+// }
